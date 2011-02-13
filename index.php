0e9238869c393e9830f5fcd2c9f0b0b508775389
Nolan Caudill <nolan@nolancaudill.com>
2011-02-12 22:37:07 -0800

    More things

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
             $user = $_SESSION['user'];
 
             print "Logged in as <a href='http://www.flickr.com/photos/{$user['nsid']}/'>{$user['username']}</a>. <a href='/logout.php'>Not you?</a>";
+            print "<br><br>";
 
             // Show next trickle photos
             $rsp = flickr_get_trickle_photos($user);
@@ -22,20 +23,24 @@
                 $rsp = $rsp['rsp'];
                 if($rsp['stat'] == 'ok') {
                     $photos = $rsp['photos']['photo'];
-                    print "<form method='post' action='/trickle'>";
-                    print "<table>";
-                    foreach($photos as $photo) {
-                        $url = "http://www.flickr.com/photos/{$photo['owner']}/{$photo['id']}/";
+                    if($photos) {
+                        print "<form method='post' action='/trickle'>";
+                        print "<table>";
+                        foreach($photos as $photo) {
+                            $url = "http://www.flickr.com/photos/{$photo['owner']}/{$photo['id']}/";
 
-                        print "<tr>";
-                        print "<td><input checked type='checkbox' name='photos[]' value='{$photo['id']}'/></td>";
-                        print "<td><a href='$url'>{$photo['title']}</a></td>";
-                        print "<td><a href='$url'><img width='{$photo['width_s']}' height='{$photo['height_s']}' src='{$photo['url_s']}'/></a></td>";
-                        print "</tr>";
+                            print "<tr>";
+                            print "<td><input checked type='checkbox' name='photos[]' value='{$photo['id']}'/></td>";
+                            print "<td><a href='$url'>{$photo['title']}</a></td>";
+                            print "<td><a href='$url'><img width='{$photo['width_s']}' height='{$photo['height_s']}' src='{$photo['url_s']}'/></a></td>";
+                            print "</tr>";
+                        }
+                        print "</table>";
+                        print "<input type='submit' name='Trickle!'/>";
+                        print "</form>";
+                    } else {
+                        print "No FlickrTrickle photos. To add them, add the tag 'flickrtrickle' to your private photos.";
                     }
-                    print "</table>";
-                    print "<input type='submit' name='Trickle!'/>";
-                    print "</form>";
                 } else {
                     print $rsp['message']; 
                 }
--- trickle.php
+++ trickle.php
@@ -7,17 +7,17 @@
     $token = $_SESSION['user']['token'];
 
     if(isset($_SESSION['user'])) {
-        foreach($_POST['photos'] as $photo) {
+        foreach($_POST['photos'] as $photo_id) {
             loadlib('flickr');
             $args = array(
                 'method' => 'flickr.photos.getInfo',
-                'photo_id' => $photo,
-                'auth_token' => $token],
+                'photo_id' => $photo_id,
+                'auth_token' => $token,
             );
             $rsp = flickr_api_call($args, true);
             if($rsp['ok'] && $rsp['rsp']['stat'] == 'ok') {
-                $rsp = $rsp['rsp'];
-                $tags = $rsp['photo']['tags']['tag'];
+                $photo = $rsp['rsp']['photo'];
+                $tags = $photo['tags']['tag'];
 
                 $tag_id = false;
                 foreach($tags as $tag) {
@@ -30,8 +30,8 @@
                 // Update date
                 $args = array(
                     'method' => 'flickr.photos.setDates',
-                    'auth_token' => $token],
-                    'photo_id' => $photo,
+                    'auth_token' => $token,
+                    'photo_id' => $photo_id,
                     'date_posted' => $time++,
                 );
                 flickr_api_call($args, true);
@@ -40,22 +40,26 @@
                 $args = array(
                     'method' => 'flickr.photos.removeTag',
                     'tag_id' => $tag_id,
-                    'auth_token' => $token],
+                    'auth_token' => $token,
                 );
                 flickr_api_call($args, true);
 
                 // Make public
                 $args = array(
                     'method' => 'flickr.photos.setPerms',
-                    'photo_id' => $photo,
+                    'photo_id' => $photo_id,
                     'is_public' => 1,
                     'is_friend' => 0,
                     'is_family' => 0,
                     'auth_token' => $token,
                 );
-                $rsp = flickr_api_call($args, true);
-                print_r($rsp);
-                print "Made it public.<br/>";
+                flickr_api_call($args, true);
+                
+                print "Trickled photo \"{$photo['title']['_content']}\"<br>";
             }
         }
     }
+
+?>
+
+<a href="/">Back to home</a>
